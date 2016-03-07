8d87b9a92629265ed2c2f1a033336e8b75f80074
Brian Gregg <bgregg@umass.edu>
2016-03-07 11:54:10 -0500

    got rid of some starter code

--- www/js/app.js
+++ www/js/app.js
@@ -49,15 +49,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
 
   .state('app.single', {
     url: '/playlists/:playlistId',
@@ -69,5 +60,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/browse');
 });
