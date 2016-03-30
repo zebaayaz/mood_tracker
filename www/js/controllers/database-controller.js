af71739655ed8a803a3afc47baa3b2c380da2eae
egendreau <egendreau254@gmail.com>
2016-03-30 15:45:29 -0400

    database-controller documented

--- www/js/controllers/database-controller.js
+++ www/js/controllers/database-controller.js
@@ -1,7 +1,11 @@
 module.controller('MyCtrl', function($scope, $cordovaSQLite) {
 
+  // SQLite database used for application
+  // plugin is from ngCordova : https://github.com/litehelpers/Cordova-sqlite-storage.git
+
   var db = $cordovaSQLite.openDB({ name: "mood_tracker_database" });
 
+  // this is example code left for the moment so others can better understand database structure
   $scope.execute = function() {
     var query = "INSERT INTO test_table (data, data_num) VALUES (?,?)";
     $cordovaSQLite.execute(db, query, ["test", 100]).then(function(res) {
