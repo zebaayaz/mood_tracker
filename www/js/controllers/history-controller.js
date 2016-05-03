55989893d3c80ae2f35603fa376f3b3fb4c17a49
rohan kapoor <rohkapoor@umass.edu>
2016-05-03 03:54:43 -0400

    added views elements to all but help. updated routes to go to login and signup screen. implemented themes

--- www/js/controllers/history-controller.js
+++ www/js/controllers/history-controller.js
@@ -1,3 +1,22 @@
-angular.module('mood_tracker.controllers').controller('historyController', function($scope, $stateParams) {
+angular.module('mood_tracker.controllers').controller('historyController', function($scope,$window, $stateParams) {
+  //get theme
+  $scope.appTheme = 'positive';
+  var selectedTheme = $window.localStorage.appTheme;
+    if (selectedTheme) {
+        $scope.appTheme = selectedTheme;
+    } else {
+        $scope.appTheme = 'positive';
+    }
 
+  //select by date  or mood
+  $scope.show = [true, false];
+  $scope.selectBy = 'Date';
+  $scope.moods = ["Happy", "Sad", "Hungry"];
+  $scope.updateSelectBy = function(selected){
+    if(selected === 'Date'){
+      $scope.show = [true, false];
+    }else{
+      $scope.show = [false, true];
+    }
+  };
 });
